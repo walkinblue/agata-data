2de6976a34344e1926388cee8ad2d49cf2df0735
走马青台 <lixihan@mac.com>
2023-06-09 19:05:21 +0800

    agata 0.0.1.0

--- command/sftp-lines.sh
+++ command/sftp-lines.sh
@@ -6,12 +6,12 @@ search_dir=/root/agatamind/agata-data/source/.
 echo "  " >  $outfile echo
 for entry in "$search_dir"/*.png
 do
-  echo "get /root/agatamind/agata-data/source/$entry /Users/mark/Coding/agatamind/agata-data/source/." >> $outfile
+  echo "get $entry /Users/mark/Coding/agatamind/agata-data/source/." >> $outfile
 done
 
 for entry in "$search_dir"/*.png
 do
-  echo "put /Users/mark/Coding/agatamind/agata-data/source/$entry /root/agatamind/agata-data/source/." >> $outfile
+  echo "put $entry /root/agatamind/agata-data/source/." >> $outfile
 done
 
 cd /root/agatamind
