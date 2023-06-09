bd5f6bb8008f794a00e4edd00d7d0e18f575a077
走马青台 <lixihan@mac.com>
2023-06-10 00:18:43 +0800

    agata 0.0.2.8

--- command/run-service.sh
+++ command/run-service.sh
@@ -1,26 +1,28 @@
-echo "Start the service  .... "
 APP_ENV=online
 
-if [ $1 == "agata-workshop-service" ]; then
-  if [ -z $2 ] || [ $2 == "start" ]; then
-    cd /root/agatamind/
-    rm -rf ./$1.jar
-    cp ./$1/target/$1.jar ./$1.jar
-    nohup java -jar ./$1.jar --spring.profiles.active=${APP_ENV} -Xms256m -Xmx1024m > /dev/null 2>&1 &
-    echo "Start $1 service successfully.  "
-  else if [ $2 == "stop" ]; then
-    pid=`ps -ef|grep $1|grep -v grep|awk '{print $3}' `
-    kill -9 ${pid}
+is_exist(){
+  pid=`ps -ef|grep $1|grep -v grep|awk '{print $2}' `
+  if [ -z "${pid}" ]; then
+   return 1
+  else
+    return 0
   fi
-fi
-
-if [ $1 == "agata-shop-service" ]; then
+}
+if [ -z $1 ]; then
+  echo "please input service name."
+else
     if [ -z $2 ] || [ $2 == "start" ]; then
-      cd /root/agatamind/
-      rm -rf ./$1.jar
-      cp ./$1/target/$1.jar ./$1.jar
-      nohup java -jar ./$1.jar --spring.profiles.active=${APP_ENV} -Xms256m -Xmx1024m > /dev/null 2>&1 &
-      echo "Start $1 service successfully.  "
+        is_exist
+        if [ $? -eq "0" ]; then
+          echo "$1 is already running. pid=${pid} ."
+        else
+          echo "Start the service  .... $1"
+          cd /root/agatamind/
+          rm -rf ./$1.jar
+          cp ./$1/target/$1.jar ./$1.jar
+          nohup java -jar ./$1.jar --spring.profiles.active=${APP_ENV} -Xms256m -Xmx1024m > /dev/null 2>&1 &
+          echo "Start $1 service successfully.  "
+        fi
     else if [ $2 == "stop" ]; then
       pid=`ps -ef|grep $1|grep -v grep|awk '{print $3}' `
       kill -9 ${pid}
