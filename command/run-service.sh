6dc77ad0d6d07f1be055bb6e7743a02a6e7e260d
走马青台 <lixihan@mac.com>
2023-06-10 00:32:54 +0800

    agata 0.0.2.8

--- command/run-service.sh
+++ command/run-service.sh
@@ -8,35 +8,55 @@ is_exist(){
     return 0
   fi
 }
+
+start(){
+  is_exist
+  if [ $? -eq "0" ]; then
+    echo "$1 is already running. pid=${pid} ."
+  else
+    echo "Start the service  .... $1"
+    cd /root/agatamind/
+    rm -rf ./$1.jar
+    cp ./$1/target/$1.jar ./$1.jar
+    nohup java -jar ./$1.jar --spring.profiles.active=${APP_ENV} -Xms256m -Xmx1024m > /dev/null 2>&1 &
+    echo "Start $1 service successfully.  "
+  fi
+}
+
+stop(){
+  is_exist
+  if [ $? -eq "0" ]; then
+    kill -9 ${pid}
+  else
+    echo "$1 is not running"
+  fi
+}
+
+status(){
+  is_exist
+  if [ $? -eq "0" ]; then
+    echo "$1 is running. Pid is ${pid}"
+  else
+    echo "$1 is NOT running."
+  fi
+}
+
 if [ -z $1 ]; then
   echo "please input app name: ./run-service.sh [APP-NAME] [start|stop|status(default)]."
 else
-    if [ $2 == "start" ]; then
-        is_exist
-        if [ $? -eq "0" ]; then
-          echo "$1 is already running. pid=${pid} ."
-        else
-          echo "Start the service  .... $1"
-          cd /root/agatamind/
-          rm -rf ./$1.jar
-          cp ./$1/target/$1.jar ./$1.jar
-          nohup java -jar ./$1.jar --spring.profiles.active=${APP_ENV} -Xms256m -Xmx1024m > /dev/null 2>&1 &
-          echo "Start $1 service successfully.  "
-        fi
-    else
-      if [ $2 == "stop" ]; then
-        is_exist
-        if [ $? -eq "0" ]; then
-          echo "kill $1. pid=${pid} ."
-          kill -9 ${pid}
-        fi
-      else
-        if [ -z $2 ] || [ $2 == "status" ]; then
-          is_exist
-          if [ $? -eq "0" ]; then
-            echo "$1 is running. pid=${pid} ."
-          fi
-        fi
-      fi
-    fi
+  case "$2" in
+  "start")
+    start
+    ;;
+  "stop")
+    stop
+    ;;
+  "status")
+    status
+    ;;
+  *)
+    usage
+    ;;
+  esac
 fi
+
