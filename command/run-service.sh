7e07d8cbf30b8c14ec0a1e8b095b75d59906ede8
走马青台 <lixihan@mac.com>
2023-06-10 00:10:36 +0800

    agata 0.0.2.7

--- command/run-service.sh
+++ command/run-service.sh
@@ -2,17 +2,27 @@ echo "Start the service  .... "
 APP_ENV=online
 
 if [ $1 == "agata-workshop-service" ]; then
-  cd /root/agatamind/
-  rm -rf ./$1.jar
-  cp ./$1/target/$1.jar ./$1.jar
-  nohup java -jar ./$1.jar --spring.profiles.active=${APP_ENV} -Xms256m -Xmx1024m > /dev/null 2>&1 &
-  echo "Start $1 service successfully.  "
+  if [ -z $2 ] || [ $2 == "start" ]; then
+    cd /root/agatamind/
+    rm -rf ./$1.jar
+    cp ./$1/target/$1.jar ./$1.jar
+    nohup java -jar ./$1.jar --spring.profiles.active=${APP_ENV} -Xms256m -Xmx1024m > /dev/null 2>&1 &
+    echo "Start $1 service successfully.  "
+  else if [ $2 == "stop" ]; then
+    pid=`ps -ef|grep $1|grep -v grep|awk '{print $3}' `
+    kill -9 ${pid}
+  fi
 fi
 
 if [ $1 == "agata-shop-service" ]; then
-  cd /root/agatamind/
-  rm -rf ./$1.jar
-  cp ./$1/target/$1.jar ./$1.jar
-  nohup java -jar ./$1.jar --spring.profiles.active=${APP_ENV} -Xms256m -Xmx1024m > /dev/null 2>&1 &
-  echo "Start $1 service successfully.  "
+    if [ -z $2 ] || [ $2 == "start" ]; then
+      cd /root/agatamind/
+      rm -rf ./$1.jar
+      cp ./$1/target/$1.jar ./$1.jar
+      nohup java -jar ./$1.jar --spring.profiles.active=${APP_ENV} -Xms256m -Xmx1024m > /dev/null 2>&1 &
+      echo "Start $1 service successfully.  "
+    else if [ $2 == "stop" ]; then
+      pid=`ps -ef|grep $1|grep -v grep|awk '{print $3}' `
+      kill -9 ${pid}
+    fi
 fi
